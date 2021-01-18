3b5cde92c4397929f0aaf10016edea0c90ead0e9
Vicktron <tamsvictorisaac@gmail.com>
2021-01-18 11:19:05 +0100

    Initial commit

---
+++ GUI.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import cv2
+
+root = Tk()
+root.title('Advanced Attendance System')
+
+
+cap = cv2.VideoCapture(0)
+my_label = Frame.image_types()
+cv2.imshow('Webcam', img)
+cv2.waitKey(1)
+
+# The Buttons
+
+# def on_button():
+#     onbutton = Button(root, b)
+
+
+
+# my_img = ImageTk.PhotoImage(Image.open('DREAM BIG.jpg'))
+
+
+root.mainloop()
